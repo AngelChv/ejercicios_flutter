add96753ff9cb05ef3564ed31b20324ee72cf562
angel.chiveg <angel.chiveg@educa.jcyl.es>
2024-11-06 14:20:21 +0100

    jsonplaceholder

---
+++ lib/jsonplaceholder_ej16.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
