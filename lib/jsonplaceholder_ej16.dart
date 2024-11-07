74291c876e0903a41923fd115668718bb0139615
angel <angel.chiveg@educa.jcyl.es>
2024-11-07 18:27:07 +0100

    Ejercicio 16 listview con datos de jsonplaceholder

--- lib/jsonplaceholder_ej16.dart
+++ lib/jsonplaceholder_ej16.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -19,10 +23,71 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
+  List<Padding> list = [];
+
+  void _initialize(List<Map<String, dynamic>> data) {
+    setState(() {
+      list = getResponseAsList(data);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    getResponse(Uri.https('jsonplaceholder.typicode.com', '/todos/'))
+        .then((data) {
+      _initialize(data);
+    });
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView con datos de JSONPlaceHolder"),
+      ),
+      body: ListView(
+        children: list,
+      ),
     );
   }
+}
+
+List<Padding> getResponseAsList(List<Map<String, dynamic>> responseList) {
+  List<Padding> list = [];
+  for (var item in responseList) {
+    list.add(
+      Padding(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text("UserId: ${item["userId"]}"),
+              ),
+              Expanded(
+                child: Text("id: ${item["id"]}"),
+              ),
+              Expanded(
+                flex: 4,
+                child: Text("title: ${item["title"]}"),
+              ),
+              Expanded(
+                child: Text("completed: ${item["completed"]}"),
+              ),
+            ],
+          )),
+    );
+  }
+  return list;
+}
+
+Future<List<Map<String, dynamic>>> getResponse(Uri uri) async {
+  List<Map<String, dynamic>> jsonResponse = [];
+  var url = Uri.https('jsonplaceholder.typicode.com', '/todos/');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    for (Map<String, dynamic> i in jsonDecode(response.body) as List<dynamic>) {
+      jsonResponse.add(i);
+    }
+  } else {
+    throw Exception("Error: status code = ${response.statusCode}");
+  }
+
+  return jsonResponse;
 }
